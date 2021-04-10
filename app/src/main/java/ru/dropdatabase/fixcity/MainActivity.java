1ccee5c93db9d0a78bb95367d9434f11088452eb
hawwyo <60163111+hawwyo@users.noreply.github.com>
2021-04-10 11:52:43 +0800

    Add files via upload

---
+++ app/src/main/java/ru/dropdatabase/fixcity/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.dropdatabase.fixcity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}x
