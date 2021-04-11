2c5ac3ef6f9a0235e4d940a129b07da3a3fce7d9
Aleksey Demakov <60163292+AleksDemakov@users.noreply.github.com>
2021-04-11 12:09:19 +0800

    Update GetPetitionDAO.java

--- app/src/main/java/ru/dropdatabase/fixcity/GetPetitionDAO.java
+++ app/src/main/java/ru/dropdatabase/fixcity/GetPetitionDAO.java
@@ -1,9 +1,22 @@
 package ru.dropdatabase.fixcity;
 
 
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,30 +24,52 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class GetPetitionDAO implements getPetition {
 
     FirebaseDatabase database;
-    int counter;
+    int counter, tmp;
     public GetPetitionDAO() {
         database = FirebaseDatabase.getInstance();
-        Query mutq = database.getReference().child("mutator");
-        int tmp = -1;
+
+        Query mutq = database.getReference("mutator");
+        tmp = -1;
         try {
-            tmp = mutq.get().getResult().getValue(Integer.class);
-            Thread.sleep(100);
+                mutq.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Log.d("mytag", "!"+snapshot.getValue(Integer.class).toString());
+                    setCounter(snapshot.getValue(Integer.class));
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {}
+            });
         }catch (Exception e){
-            System.out.println(e.getMessage());
+            Log.d("mytag", e.toString());
         }
+    }
+    void setCounter(int tmp) {
+        this.tmp = tmp;
+
         if(tmp == -1){
             counter = 0;
             database.getReference("mutator").setValue(counter);
         }else{
             counter = tmp;
+            Log.d("mytag", "tmp is not null " + Integer.toString(counter));
         }
     }
-    int incrementCounter(){
+
+    private int incrementCounter(){
         int res = counter;
         database.getReference("mutator").setValue(counter + 1);
         counter = counter + 1;
         return res;
     }
+
+    @Override
+    public void update(int petitionId, Petition petition) {
+        DatabaseReference myRef = database.getReference("petitions").child(petition.getTitle());
+        myRef.setValue(petition);
+        saveComments(petition.getTitle(), petition.getComments());
+    }
+
     @Override
     public void save(Petition petition) {
 
